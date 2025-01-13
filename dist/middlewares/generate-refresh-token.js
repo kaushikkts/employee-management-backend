b81e19bcc2c361b8bbd7b7049bb7b095169733e1
kaushikkts <105159350+kaushikkts@users.noreply.github.com>
2025-01-13 17:08:18 +0530

    remove dist

--- dist/middlewares/generate-refresh-token.js
+++
@@ -1,5 +0,0 @@
-// import jwt from "jsonwebtoken";
-//
-// export default function generateRefreshToken(user: { name: string }) {
-//   return jwt.sign(user, process.env.REFRESH_TOKEN_SECRET, { expiresIn: "30d" });
-// }
